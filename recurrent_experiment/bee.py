b35f99c8bf4264ef27f2787f251d3d41e2062e94
Alex Simms <alexmsimms@gmail.com>
2014-04-20 00:11:10 -0400

    Maybe this OO fad is better than I thought

---
+++ recurrent_experiment/bee.py
@@ -0,0 +1,35 @@
+class Bee(object):
+    def __init__(self, chromo):
+        self.chromo = chromo
+        self.inputs    = [()]
+        self.outputs   = [(0)]
+        self.fitnesses = [.5]
+
+    def was_selfish(self):
+        return self.outputs[-1] <= 0
+
+    def was_altruistic(self):
+        return self.outputs[-1] > 0
+
+    def found_nectar(self):
+        return self.outputs[-1][0]
+
+    def set_fitness(self, fitness):
+        self.fitnesses.append(fitness)
+
+    def avg_fitness(self):
+        choice_fitnesses = self.fitnesses[1:]
+        return sum(choice_fitnesses) / len(choice_fitnesses)
+
+    def evaluate(self, hive_nectar):
+        brain = nn.create_phenotype(chromo)
+        found_nectar = random.random()
+        ins = (found_nectar, 
+               self.outputs[-1][0], 
+               self.fitness[-1][0], 
+               hive_nectar)
+        brain.flush()
+        outs = brain.sactivate(ins)
+        self.inputs.append(ins)     
+        self.outputs.append(outs)
+
--- recurrent_experiment/evolveHive.py
+++ recurrent_experiment/evolveHive.py
@@ -2,11 +2,12 @@ from neat import config, population, chromosome, genome, visualize
 from neat.nn import nn_pure as nn
 import random
 import plot
+from bee import BEE
 out = open("log.csv", 'w')
 hive_life = open("hive.csv", 'w')
 exp = open("neg.txt", 'w')
 hive_nectar = 10.0
-
+week_length = 3
 
 def main():
     config.load('NEATconfig')
@@ -23,13 +24,17 @@ def main():
     plot.plot_hive_vitality()
 
 def eval_fitness(population):
-    day = []
-    for chromo in population:
-        day.append(eval_individual(chromo))
+    hive = [Bee(chromo) for chromo in population]
+    for day in week_length:
+        one_day(hive)
+    for i in range(len(population)):
+        population[i].fitness = hive[i].avg_fitness()
 
-    pool = [bee[0] for bee in day if bee[1] < 0]
+def one_day(hive):
+    for bee in hive:
+        bee.evaluate(hive_nectar)
 
-
+    pool = [bee.found_nectar() for bee in hive if bee.was_altruistic()]
 
     nectar_brought_in = sum(pool)
 
@@ -54,21 +59,21 @@ def eval_fitness(population):
     altruistic = 0
     selfish = 0
 
-    for chromo, production in zip(population, day):
-        if production[1] < 0:
-            chromo.fitness = avg * modifier
+    for bee in hive:
+        if bee.was_altruistic():
+            bee.set_fitness(avg * modifier)
             altruistic+=1
         else:
-            chromo.fitness = production[0] * modifier  #/2
+            bee.set_fitness(bee.found_nectar() * modifier)  #/2
             selfish+=1
     out.write("{},{}\n".format(altruistic, selfish))
 
-def eval_individual(chromo):
+def eval_individual(chromo, last_choice, last_fitness, hive_nectar):
     brain = nn.create_phenotype(chromo)
     found_nectar = random.random()
 
     brain.flush()
-    output = brain.sactivate([found_nectar])
+    output = brain.sactivate([found_nectar, last_choice, last_fitness, hive_nectar])
     exp.write(str(output[0])+'\n')
     return (found_nectar, output[0])
 
