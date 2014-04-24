e5b30db52653ae4a96af3bf5d96c9b6d72ef8764
Alex Simms <asimms1@cs.swarthmore.edu>
2014-04-23 22:14:31 -0400

    Better activation function'

--- recurrent_experiment/bee.py
+++ recurrent_experiment/bee.py
@@ -1,5 +1,7 @@
 from neat.nn import nn_pure as nn
 import random
+import matplotlib.pyplot as plot
+
 class Bee(object):
     def __init__(self, chromo):
         self.chromo = chromo
@@ -8,10 +10,10 @@ class Bee(object):
         self.fitnesses = [.5]
 
     def was_selfish(self):
-        return self.outputs[-1][0] <= 0
+        return self.outputs[-1][0] <= .5
 
     def was_altruistic(self):
-        return self.outputs[-1][0] > 0
+        return self.outputs[-1][0] > .5
 
     def found_nectar(self):
         return self.inputs[-1][0]
@@ -23,7 +25,19 @@ class Bee(object):
         choice_fitnesses = self.fitnesses[1:]
         return sum(choice_fitnesses) / len(choice_fitnesses)
 
+    def choices(self):
+        return zip([item[0] for item in self.inputs[1:]], 
+                   [item[0] for item in self.outputs[1:]])
+
     def evaluate(self, hive_nectar):
+        if hive_nectar > 10:
+            adjusted_nectar = 1
+        elif hive_nectar < 0:
+            adjusted_nectar = 0
+        else:
+            adjusted_nectar = hive_nectar / 10
+        # adjusted_nectar =
+
         brain = nn.create_phenotype(self.chromo)
         found_nectar = random.random()
         ins = (found_nectar, 
@@ -31,7 +45,6 @@ class Bee(object):
                self.fitnesses[-1], 
                hive_nectar)
         brain.flush()
-        outs = brain.sactivate(ins)
+        outs = brain.pactivate(ins)
         self.inputs.append(ins)     
         self.outputs.append(outs)
-        
--- recurrent_experiment/evolveHive.py
+++ recurrent_experiment/evolveHive.py
@@ -3,11 +3,13 @@ from neat.nn import nn_pure as nn
 import random
 import plot
 from bee import Bee
+import cPickle as pickle
 out = open("log.csv", 'w')
 hive_life = open("hive.csv", 'w')
-exp = open("neg.txt", 'w')
 hive_nectar = 10.0
 week_length = 7
+to_pickle = []
+
 
 def main():
     config.load('NEATconfig')
@@ -20,22 +22,25 @@ def main():
               checkpoint_interval=None, checkpoint_generation=5)
     out.close()
     hive_life.close()
+    pickle.dump(to_pickle, open("pickle.p", "wb"))
     plot.plot_bee_pops()
     plot.plot_hive_vitality()
 
 def eval_fitness(population):
+    generation = []
     hive = [Bee(chromo) for chromo in population]
     for day in xrange(week_length):
         one_day(hive)
     for i in xrange(len(population)):
+        generation.append(hive[i].choices())
         population[i].fitness = hive[i].avg_fitness()
+    to_pickle.append(generation)
 
 def one_day(hive):
     global hive_nectar
 
     for bee in hive:
         bee.evaluate(hive_nectar)
-
     pool = [bee.found_nectar() for bee in hive if bee.was_altruistic()]
 
     nectar_brought_in = sum(pool)
@@ -48,9 +53,10 @@ def one_day(hive):
 
     avg = nectar_brought_in * 0.9/len(pool) if len(pool) > 0 else 0
 
+    # modifier = 1
     if hive_nectar > 10:
         modifier = 1
-    elif hive_nectar > 0:
+    elif hive_nectar > 5:
         modifier = hive_nectar /10
     else:
         modifier = .001 
